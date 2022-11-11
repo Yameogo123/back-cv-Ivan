e3fba05555aa0a5ff414474aa2fef4f7b3e34415
Yameogo123 <58187516+Yameogo123@users.noreply.github.com>
2022-11-11 18:24:31 +0100

    Update router.js

--- routing/router.js
+++ routing/router.js
@@ -13,35 +13,35 @@ const skillCtrl= require("../controller/SkillController");
 const userCtrl= require("../controller/UserController");
 
 
-router.get('/adresse/all', auth, addCtrl.get);
+router.get('/adresse/all', addCtrl.get);
 router.post('/adresse/new', auth, addCtrl.add);
 router.put('/adresse/update', auth, addCtrl.update);
 
-router.get('/certification/all', auth, certifCtrl.get);
+router.get('/certification/all', certifCtrl.get);
 router.post('/certification/new', auth, certifCtrl.add);
 router.put('/certification/update', auth, certifCtrl.update);
 router.delete('/certification/delete/:id', auth, certifCtrl.delete);
 
-router.get('/experience/all', auth, experienceCtrl.get);
+router.get('/experience/all', experienceCtrl.get);
 router.post('/experience/new', auth, experienceCtrl.add);
 router.put('/experience/update', auth, experienceCtrl.update);
 router.delete('/experience/delete/:id', auth, experienceCtrl.delete);
 
-router.get('/formation/all', auth, formationCtrl.get);
+router.get('/formation/all', formationCtrl.get);
 router.post('/formation/new', auth, formationCtrl.add);
 router.put('/formation/update', auth, formationCtrl.update);
 router.delete('/formation/delete/:id', auth, formationCtrl.delete);
 
-router.get('/message/all', auth, messCtrl.get);
+router.get('/message/all', messCtrl.get);
 router.post('/message/new', auth, messCtrl.add);
 router.delete('/message/delete/:id', auth, messCtrl.delete);
 
-router.get('/projet/all', auth, projetCtrl.get);
+router.get('/projet/all', projetCtrl.get);
 router.post('/projet/new', auth, projetCtrl.add);
 router.put('/projet/update', auth, projetCtrl.update);
 router.delete('/projet/delete/:id', auth, projetCtrl.delete);
 
-router.get('/skill/all', auth, skillCtrl.get);
+router.get('/skill/all', skillCtrl.get);
 router.post('/skill/new', auth, skillCtrl.add);
 router.put('/skill/update', auth, skillCtrl.update);
 router.delete('/skill/delete/:id', auth, skillCtrl.delete);
