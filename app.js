e7e2479c3d788d4bd43c0455ceb2758f104067e3
Yameogo123 <58187516+Yameogo123@users.noreply.github.com>
2022-11-11 17:59:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser= require("body-parser")
 
 const app= express()
 
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
