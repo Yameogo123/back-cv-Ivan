4a9b50fd552c1d43dce0fbca10291d44a8651d48
Yameogo123 <58187516+Yameogo123@users.noreply.github.com>
2022-11-12 20:24:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const router= require("./routing/router")
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, OPTIONS, DELETE");
     next();
 })
 
